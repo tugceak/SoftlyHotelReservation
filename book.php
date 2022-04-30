ddd18f79fc6f8f24bca66f12822ebde06ede5fec
Tuğçe Aksoy <tugceaksy60@gmail.com>
2022-04-30 20:58:11 +0300

    Initial commit

---
+++ book.php
@@ -0,0 +1,5 @@
+
+<?php  
+echo "Merhaba";  // Ekrana Merhaba yazar  
+echo 'Merhaba';  // Ekrana Merhaba yazar  
+?>  
---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$database = "booking";
+$username = "tugceaksoy";
+$password = "tugce2000";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . 
+mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
